7b53d774a6ae4a701852ea17b0ab72889c7f2dc1
Eric Masi <eric@gitmasi.com>
2019-06-18 08:11:09 -0400

    add Logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation';
+import Logo from './components/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
-      <FacialRecognition /> */}
+      <Logo />
+      {/* <ImageLinkForm /> */}
+      {/* <FacialRecognition /> */}
     </div>
   );
 }
