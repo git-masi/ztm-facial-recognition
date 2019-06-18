fbf4737bd90bd1732369051798ac277be718afde
Eric Masi <eric@gitmasi.com>
2019-06-18 18:59:34 -0400

    add styles

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Tilt from 'react-tilt'
+import styles from './Logo.module.css';
 
 const Logo = () => {
   return (
-    <div></div>
+    <div>
+      <Tilt className={styles.tilt} options={{ max : 45 }} style={{ height: 150, width: 150 }} >
+        <div className={styles.tiltInner}> <span role="img" aria-label="just an alien emoji">👽</span></div>
+      </Tilt>
+    </div>
   )
 }
 
