a89bb2694eea3561a3a90f9720475908c5669954
Eric Masi <eric@gitmasi.com>
2019-06-18 20:19:53 -0400

    add Particles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import ImageLinkForm from './components/ImageLinkForm';
@@ -8,6 +9,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+       <Particles
+          className="particleStyles"
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 400,
+                }
+              }
+            }
+          }} 
+        />
       <Navigation />
       <Logo />
       <Rank />
