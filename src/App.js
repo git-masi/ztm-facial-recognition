f090291ac8e1ea8403138992972b65e324c54356
Eric Masi <eric@gitmasi.com>
2019-06-19 07:15:46 -0400

    add state and event handlers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation';
 import Logo from './components/Logo';
@@ -6,30 +6,47 @@ import ImageLinkForm from './components/ImageLinkForm';
 import Rank from './components/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-       <Particles
-          className="particleStyles"
-          params={{
-            particles: {
-              number: {
-                value: 80,
-                density: {
-                  enable: true,
-                  value_area: 400,
+class App extends Component {
+  state = {
+    input: '',
+  }
+
+  inputHandler = (e) => {
+    console.log(e.target.value);
+  }
+
+  buttonSubmitHandler = () => {
+    console.log('click');
+  }
+
+  render () {
+    return (
+      <div className="App">
+         <Particles
+            className="particleStyles"
+            params={{
+              particles: {
+                number: {
+                  value: 80,
+                  density: {
+                    enable: true,
+                    value_area: 400,
+                  }
                 }
               }
-            }
-          }} 
+            }} 
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          inputHandler={this.inputHandler}
+          buttonSubmitHandler={this.buttonSubmitHandler}
         />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FacialRecognition /> */}
-    </div>
-  );
+        {/* <FacialRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm.js
+++ src/components/ImageLinkForm.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './ImageLinkForm.module.css';
 
-const ImageLinkForm = () => {
+const app = 'c73b0a1735814a7b8be81b3640ccf8a5';
+
+const ImageLinkForm = ({ inputHandler, buttonSubmitHandler}) => {
   return (
     <div className={styles.container}>
       <p>Enter the URL of a picture and the app will detect human faces.</p>
       <div className={styles.inputContainer}>
-        <input type="text"></input>
-        <button>Detect!</button>
+        <input type="text" onChange={inputHandler}></input>
+        <button onClick={buttonSubmitHandler}>Detect!</button>
       </div>
     </div>
   )
